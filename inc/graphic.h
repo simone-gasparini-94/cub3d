cdd5c327973e7d40dfd824ede83f054d9a311219
Simone Gasparini <simone.gasparini.2@gmail.com>
2025-12-08 11:42:34 +0100

    Render 2d map

--- inc/graphic.h
+++ inc/graphic.h
@@ -1,14 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphic.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 10:30:54 by sgaspari          #+#    #+#             */
+/*   Updated: 2025/12/08 11:03:52 by sgaspari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GRAPHIC_H
 # define GRAPHIC_H
 
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_l;
+	int		endian;
+}	t_img;
+
 typedef struct s_grph
 {
+	t_img	img;
 	void	*mlx;
 	void	*win;
 	int		texture_width;
 	int		texture_height;
 	int		window_width;
 	int		window_height;
+	double	tile_width;
+	double	tile_height;
 }			t_grph;
 
 int			init_mlx(t_data *data);
--- src/render.c
+++ src/render.c
@@ -1,8 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 10:13:44 by sgaspari          #+#    #+#             */
+/*   Updated: 2025/12/08 11:38:03 by sgaspari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "data.h"
+#include "libft.h"
 #include <mlx.h>
+#include <stdio.h>
+
+static void	render_map2D(t_data *data);
+static int	put_pixel(t_img *img, int x, int y, uint32_t color);
 
 void    render(t_data   *data)
 {
-    mlx_put_image_to_window(data->grph.mlx, data->grph.win, data->tex[NO].texture, 0, 0);
-    mlx_put_image_to_window(data->grph.mlx, data->grph.win, data->tex[SO].texture, 256, 0);
+	render_map2D(data);
+}
+
+static void	render_map2D(t_data *data)
+{
+	double	tile_width;
+	double	tile_height;
+	int	i;
+	int	j;
+
+	data->grph.img.img = mlx_new_image(data->grph.mlx,
+			data->grph.window_width, data->grph.window_height);
+	data->grph.img.addr = mlx_get_data_addr(data->grph.img.img,
+			&data->grph.img.bpp, &data->grph.img.line_l,
+			&data->grph.img.endian);
+	tile_width = 20;
+	tile_height = 20;
+
+	printf("tile width = %f, tile height = %f\n", tile_width, tile_height);
+	i = 0;
+	while (i < tile_height * data->map.rows)
+	{
+		j = 0;
+		while (j < tile_width * data->map.cols)
+		{
+			if ((j / (int)tile_width) < (int)ft_strlen(data->map.matrix[i / (int)tile_height]) 
+					&& data->map.matrix[i / (int)tile_height][j / (int)tile_width] == '1')
+				put_pixel(&data->grph.img, j, i, 0xFFFFFF);
+			else if ((j / (int)tile_width) < (int)ft_strlen(data->map.matrix[i / (int)tile_height])
+					&& data->map.matrix[i / (int)tile_height][j / (int)tile_width] == data->map.player_char)
+				put_pixel(&data->grph.img, j, i, 0xFF0000);
+			else
+				put_pixel(&data->grph.img, j, i, 0x000000);
+			j++;
+		}
+		i++;
+	}
+	mlx_put_image_to_window(data->grph.mlx, data->grph.win, data->grph.img.img, 0, 0);
+}
+
+static int	put_pixel(t_img *img, int x, int y, uint32_t color)
+{
+	char		*px;
+
+	px = img->addr + (y * img->line_l + x * (img->bpp / 8));
+	*(uint32_t *)px = color;
+	return (0);
 }
